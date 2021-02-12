ac3c4475a289af47d8bcab8e8b25dd43ae1d5b18
URIAN VIERA <urian1213viera@gmail.com>
2021-02-12 09:48:15 -0500

    Update config.php
    modificando conexion

--- config.php
+++ config.php
@@ -6,11 +6,4 @@ $basededatos = "mivotos";
 $con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
 $db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
 
-/*$usuario = "epiz_21499340";
-$password = "gooYes9FLVMT";
-$servidor = "sql302.epizy.com";
-$basededatos = "epiz_21499340_victimas";
-$con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
-$db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
-*/
 ?>
